83083640404540f0594ccdc6a75237a725f9251b
Drew Wyatt <drew.j.wyatt@gmail.com>
2016-10-02 00:37:54 -0500

    added file drop

--- src/containers/home.tsx
+++ src/containers/home.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 
-class Home extends React.Component<void, void> {
+export interface HomeProps {
+    fileName: string;
+    link: string;
+}
+
+class Home extends React.Component<HomeProps, void> {
     render(): JSX.Element {
         return (
-            <h2>HOME</h2>
+            <section>
+                <input type='file' onChange={this._onFileChange.bind(this)} />
+            </section>
         );
+    }
+
+    private _onFileChange(event: Event): void {
+        event.preventDefault();
+        const input = event.target as HTMLInputElement;
+        if (this._filesExist(input)) {
+            const file = input.files[0];
+            console.log(file);
+        }
+    }
+
+    private _filesExist(input: HTMLInputElement): boolean {
+        return !!(input && input.files && input.files.length);
     }
 }
 
-export default Home;
+export default connect(() => ({}))(Home);

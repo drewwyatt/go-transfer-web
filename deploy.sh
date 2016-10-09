a03f66803b69d8a76552f42e200fd2854c2c63a0
Drew Wyatt <drew.j.wyatt@gmail.com>
2016-10-09 00:25:53 -0500

    added actual deploy stuff

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,21 @@
-echo "If you can see this, the deploy script would be running right now..."
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+cd dist
+
+git init
+git config user.name "Drew Wyatt"
+git config user.email "drew.j.wyatt@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/drewwyatt/go-transfer-web.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages

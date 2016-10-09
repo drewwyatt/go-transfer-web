61acb810b021044d0e308ab028466d4663cbbf3f
Drew Wyatt <drew.j.wyatt@gmail.com>
2016-10-09 15:18:31 -0500

    can check for file.

--- src/middleware/api.ts
+++ src/middleware/api.ts
@@ -5,6 +5,7 @@
 
 import { Store, Action } from 'redux';
 import { Types, Creators } from '../actions';
+import { Availability } from '../models';
 
 const { FileActions } = Types;
 
@@ -13,7 +14,6 @@ type Next = (action: Action) => any; // TODO
 const API_BASE = 'https://go-transfer.herokuapp.com';
 
 function postFile(file: File, fileName: string): Promise<Response> {
-
     return fetch(`${API_BASE}/${fileName}`, {
         method: 'POST',
         body: file
@@ -38,6 +38,26 @@ function handlePostFileError(next: Next): (error: Error) => Next {
     };
 }
 
+function checkAvailability(filename: string): Promise<Response> {
+    return fetch(`${API_BASE}/exists/${filename}`);
+}
+
+function handleFileAvailable(next: Next): (response: Response) => Next {
+    return function (response: Response) {
+        if (response.status !== 200) {
+            throw new Error('File not available.');
+        }
+
+        return next(Creators.File.setAvailability(Availability.AVAILABLE));
+    }
+}
+
+function handleFileNotAvailable(next: Next): (error: Error) => Next {
+    return function (error) {
+        return next(Creators.File.setAvailability(Availability.NOT_AVAILABLE));
+    }
+}
+
 export default (store: Store<any>) => (next: Next) => (action: Action) => {
     next(action);
     switch (action.type) {
@@ -47,5 +67,11 @@ export default (store: Store<any>) => (next: Next) => (action: Action) => {
             return postFile(file, name)
                 .then(handlePostFileSuccess(next))
                 .catch(handlePostFileError(next));
+        case FileActions.ActionType.CHECK_FILE_AVAILABILITY:
+            const { fileName } = (action as Types.FileActions.CheckFileAvailability).payload;
+
+            return checkAvailability(fileName)
+                .then(handleFileAvailable(next))
+                .catch(handleFileNotAvailable(next));
     }
 }
